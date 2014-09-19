5ec9c771787d2d42ce4b515eafe3fa4b9041aca5
Steven Goodwin <hardenbroecke@gmail.com>
2014-09-19 16:07:19 -0700

    initial api

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var json = require('./list_api.json');
+http.createServer(function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "http://airbrush.wetpaint.com:3000");
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(json));
+}).listen(8080);
+console.log('http://airbrush.wetpaint.com:8080/');
